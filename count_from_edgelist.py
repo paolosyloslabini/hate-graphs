d80159cecc9df06c7f174fa90202d3eadcf49b01
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-05-11 19:44:55 +0200

    Update count_from_edgelist.py

--- count_from_edgelist.py
+++ count_from_edgelist.py
@@ -22,8 +22,10 @@ with open(input_file, "r") as infile:
     with open(output_bigraph, "w") as outfile:
     
     
-        tmp_dict = {}
-        last_node = -1
+        tmp_n2s = []
+        tmp_vals = []
+        last_n1 = -1
+        last_n2 = -1
         
         for line in infile.readlines():
             line = line.split(" ")
@@ -31,13 +33,17 @@ with open(input_file, "r") as infile:
             n2 = line[1]
             val = line[2]
             
-            if n1 == last_node:
-                if n2 not in tmp_dict:
-                    tmp_dict[n2] = val
+            if n1 == last_n1:
+                if n2 != last_n2:
+                    tmp_n2s.append(n2)
+                    tmp_vals.append(val)
                 else:
-                    tmp_dict[n2] += val
-            
-            if n1 != last_node:
-                outfile.writelines([f"{last_node} {n2} {val}" for n2, val in tmp_dict.items()])
-                last_node = n1
-                tmp_dict = {}
+                    tmp_vals[-1] += val    
+                    
+                    
+            if n1 != last_n1:
+                outfile.writelines([f"{last_n1} {tmp_n2s[i]} {tmp_vals[i]}" for i in range(len(tmp_vals))])
+                tmp_n2s = []
+                tmp_vals = []
+                last_n1 = -1
+        last_n2 = -1
